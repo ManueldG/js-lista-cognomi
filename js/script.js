9bab704b63c47629bab9b2015177589ad64056ca
Manuel della Gala <manuel.dellagala@gmail.com>
2021-03-24 18:03:43 +0100

    lista

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ Consegna
 4. scrivi anche la posizione "umana", 1 oppure 2 oppure 3 oppure ecc... (non 0 oppure 1 oppure 2 oppure ecc.. tipici degli array), del nuovo utente inserito sopra all'interno dell'array, dopo l'ordinamento alfabetico
 Consigli del giorno:
 Consultiamo la documentazione W3Schools o MDN per trovare i metodi javascript che possono esserci utili.*/
+var nome = '';
+var lista = ["rossi", "bianchi","grigi","capone"];
+
+do {
+    nome = prompt("cognome").toLowerCase();
+}while((nome.lenght < 0) );
+
+lista.push(nome);
+console.log(lista);
+lista.sort();
+console.log(lista);
+
+for (var i=0; i<lista.length; i++)
+    document.getElementById("lista").innerHTML += "<li>"+lista[i]+"</li>";
